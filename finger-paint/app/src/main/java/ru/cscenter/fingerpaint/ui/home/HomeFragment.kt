baf28261a288f413b0f1467e9d3094f0ca124f02
Maxim Zuev <max-zu99@mail.ru>
2019-11-14 00:23:34 +0300

    Add home screen

---
+++ finger-paint/app/src/main/java/ru/cscenter/fingerpaint/ui/home/HomeFragment.kt
@@ -0,0 +1,30 @@
+package ru.cscenter.fingerpaint.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import ru.cscenter.fingerpaint.R
+
+class HomeFragment : Fragment() {
+
+    private lateinit var homeViewModel: HomeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        homeViewModel =
+            ViewModelProviders.of(this).get(HomeViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
+
+        val currentNameTextView: TextView = root.findViewById(R.id.current_name_text_view)
+        currentNameTextView.text = homeViewModel.currentName
+
+        return root
+    }
+}
