4a2de98381494250f9a5ea7e128f7a0c3a493e7e
krock21ru <krock21rus@gmail.com>
2020-01-11 20:49:46 +0300

    Added loading png format

--- finger-paint/app/src/main/java/ru/cscenter/fingerpaint/ui/games/images/ImageLoader.kt
+++ finger-paint/app/src/main/java/ru/cscenter/fingerpaint/ui/games/images/ImageLoader.kt
@@ -3,27 +3,41 @@ package ru.cscenter.fingerpaint.ui.games.images
 import android.content.res.Resources
 import android.graphics.Bitmap
 import android.graphics.Color
+import android.util.Log
+import androidx.core.graphics.get
+import ru.cscenter.fingerpaint.ui.games.base.DrawingView
+import java.util.*
 
-private const val COMPRESSION_RATE = 8
+private const val GET_IMAGE_DEFAULT_COMPRESSION_RATE = 1
+private const val GET_IMAGE_COMPRESSED_DEFAULT_COMPRESSION_RATE = 8
 
 fun getImage(
     resourceId: Int,
     resources: Resources,
     color: Int = Color.BLACK
-): (Int, Int) -> Bitmap = { width, height ->
-    getBitmapFromResource(width, height, resources, resourceId)
-}
+): (Int, Int) -> Bitmap =
+    getImageCompressed(resourceId, resources, color, GET_IMAGE_DEFAULT_COMPRESSION_RATE)
 
 fun getImageCompressed(
     resourceId: Int,
     resources: Resources,
-    color: Int = Color.BLACK,
-    compressionRate: Int = COMPRESSION_RATE
+    color: Int = Color.BLACK, // important for black/white(good/bad) pixels
+    compressionRate: Int = GET_IMAGE_COMPRESSED_DEFAULT_COMPRESSION_RATE
 ): (Int, Int) -> Bitmap = { width, height ->
-    getBitmapFromResource(
+    val bitmap = getBitmapFromResource(
         width / compressionRate,
         height / compressionRate,
         resources,
         resourceId
     )
+
+    for (y in 0 until bitmap.height) {
+        for (x in 0 until bitmap.width) {
+            if (Color.alpha(bitmap.getPixel(x, y)) != 0) {
+                bitmap.setPixel(x, y, color)
+            }
+        }
+    }
+
+    bitmap
 }
