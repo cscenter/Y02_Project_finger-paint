376a1c3aa9eea13bbc2ea59048927d533e4a18d3
zuevmaxim <max-zu99@mail.ru>
2020-03-11 22:27:46 +0300

    Fix permission denied error (#53)

--- finger-paint/app/src/main/java/ru/cscenter/fingerpaint/service/viewexport.kt
+++ finger-paint/app/src/main/java/ru/cscenter/fingerpaint/service/viewexport.kt
@@ -1,16 +1,25 @@
 package ru.cscenter.fingerpaint.service
 
+import android.Manifest
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
 import android.net.Uri
 import android.provider.MediaStore
 import android.view.View
+import androidx.core.content.ContextCompat
+import ru.cscenter.fingerpaint.MainApplication
+import ru.cscenter.fingerpaint.R
 
 
 fun exportViewToFile(activity: Activity, view: View, title: String) {
+    if (ContextCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+        MainApplication.synchronizeController.info(activity.getString(R.string.export_permission_message))
+        return
+    }
     val bitmap = createBitmap(view)!!
     @Suppress("DEPRECATION")
     val path = MediaStore.Images.Media.insertImage(activity.contentResolver, bitmap, title, null)
